5e1522145cb982113efc6681ba97cca378d7ef4a
charaf <celadraoui.charaf@gmail.com>
2026-09-17 22:10:00 +0100

    updtaed challenges 1 to 4 in variables

---
+++ day1/variables/challenge2.js
@@ -0,0 +1,5 @@
+let Mad = 2200
+let Eur = Mad/11
+
+console.log("budget en Mad :", Mad)
+console.log("budget en Eur :", Eur)
---
+++ day1/variables/challenge3.js
@@ -0,0 +1,5 @@
+let GB = 10
+let MB = GB * 1024
+
+console.log("stockage :", GB , "GB")
+console.log("resulat :", MB, 'MB')
