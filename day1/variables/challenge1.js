174e79274f08051128a826bfcf026f3df3e13f0f
charaf <celadraoui.charaf@gmail.com>
2026-09-17 22:33:56 +0100

    Update challenge1

--- day1/variables/challenge1.js
+++ day1/variables/challenge1.js
@@ -1,11 +1,11 @@
-let Name = "charaf"
+let player = "charaf"
 let level = 12
 let age = 21
 let score = 4500
 let country = "Morocco"
 
 
-console.log("Player name is :" , Name)
+console.log("Player name is :" , player)
 console.log("Playr level is :" , level)
 console.log("Player age is :" , age)
 console.log("Player score is :" , score)
