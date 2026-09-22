f43cd6c7e12f665b47a856ab4e3c11a7921a785a
Asharfdev <87210166+Asharfdev@users.noreply.github.com>
2026-09-22 12:44:17 +0200

    Delete day3/ch1.js

--- day3/ch1.js
+++
@@ -1,8 +0,0 @@
-// const numbers = [10, 20, 30];
-// function  displayNumbers(){
-//      for (let i = 0 ; i < numbers.length ; i++ ){
-
-//              console.log(numbers[i]);
-//     }
-// }
-// displayNumbers();
